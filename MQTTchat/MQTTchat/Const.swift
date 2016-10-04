82e551f4fbea3a5c37187abe048b9614530646bc
kenta_okamura <side.junktown@gmail.com>
2016-10-04 18:30:42 +0900

    mqtt connect test

---
+++ MQTTchat/MQTTchat/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  MQTTchat
+//
+//  Created by kenta on 2016/10/04.
+//  Copyright © 2016年 sidepelican. All rights reserved.
+//
+
+import Foundation
+
+enum Const {
+    static let host         = "hostname"
+    static let port: Int32  = 1234
+    
+    // etc ...
+}
